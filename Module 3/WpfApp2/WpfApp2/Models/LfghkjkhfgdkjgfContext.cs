f0e3764d7285482f8cefd092d828a367cade28d2
unknown <egor.novikov666@bk.ru>
2025-06-18 23:15:14 +0300

    исправления

--- Module 3/WpfApp2/WpfApp2/Models/LfghkjkhfgdkjgfContext.cs
+++ Module 3/WpfApp2/WpfApp2/Models/LfghkjkhfgdkjgfContext.cs
@@ -27,7 +27,7 @@ public partial class LfghkjkhfgdkjgfContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=1111;Database=lfghkjkhfgdkjgf");
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=111;Database=lfghkjkhfgdkjgf");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- Module 4/TestKeys/TestKeys/obj/Debug/net8.0-windows/App.g.cs
+++ Module 4/TestKeys/TestKeys/obj/Debug/net8.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace TestKeys {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace TestKeys {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
         public static void Main() {
             TestKeys.App app = new TestKeys.App();
             app.InitializeComponent();
